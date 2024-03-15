207471115000c6b6066ce39dac9260ba348c5d8a
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-03-15 16:38:16 +0500

    add index and app.js files

---
+++ app.js
@@ -0,0 +1,51 @@
+//var username ="Sadaqat Barkat"
+//alert(username)
+
+
+
+//var myName="Sadakat Barkat Katto"
+//alert(myName)
+
+
+//var message="Hello World"
+//alert(message)
+
+
+
+
+
+//var age ="15 years old"
+//alert(age)
+
+
+//var studentData="Certified Mobile Appliacation Development"
+//alert(studentData)
+
+
+
+//var food ="PIZZA \n PIZZ \n PIZ \n PI \n P"
+//alert(food)
+
+
+//var email=" My eamil address is sadaqatbarkat1@gmil.com";
+//alert(email)
+
+
+
+
+
+
+//var book="I am trying to learn from the Book A smarter way to learn JavaScript"
+//alert(book)
+
+
+
+
+//document.write("Yah! I can write HTML content through JavaScript")
+
+
+//var string="--------------------------------@#$%^&*--------------------------------"
+//alert(string)
+
+
+
